174d4f23c1bf1152b0f0699b95e821f93057a675
Guilherme Monni <gmonni20@gmail.com>
2025-10-20 20:47:27 -0300

    fix api

--- server.js
+++ server.js
@@ -10,23 +10,23 @@ await server.register(cors, {
   origin: '*' //take domain front-end
 })
 
-server.get('/climate/:city', async (req, rep) => {
-  const { lat, lon } = req.params;
-  const apiKey = process.env.WEATHER_API;
+server.get('/climate/:lat/:lon', async (req, rep) => {
+  const { lat, lon } = req.params
+  const apiKey = process.env.WEATHER_API
 
   try {
     const res = await fetch(
-      `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${lat},${lon})}&aqi=yes&lang=pt-br`
+      `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${lat},${lon}&aqi=no`
     );
     
     if (!res.ok) {
-      return rep.status(404).send({ error: 'Cidade não encontrada!' });
+      return rep.status(404).send({ error: 'Cidade não encontrada!' })
     }
 
-    const data = await res.json();
-    return rep.status(200).send(data);
+    const data = await res.json()
+    return rep.status(200).send(data)
   }catch (err) {
-    return rep.status(500).send({ error: 'Erro ao buscar clima!' });
+    return rep.status(500).send({ error: 'Erro ao buscar clima!' })
   }
 });
 
