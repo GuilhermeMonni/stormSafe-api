277a94ed7d3b1a876be0351c3361fc285ae80cba
Guilherme Monni <gmonni20@gmail.com>
2025-10-20 19:39:25 -0300

    fix server

--- server.js
+++ server.js
@@ -1,28 +1,49 @@
-import fastify from "fastify"
+import fastify from 'fastify'
 import dotenv from 'dotenv'
+import cors from '@fastify/cors'
 
 dotenv.config()
 
-const server = fastify()
+const server = fastify({ logger: true })
 const apiKey = process.env.WEATHER_API
 
-server.post('/searchClimate', async (req, rep) => {
-    //get infos 
-    const { city } = req.body
+await server.register(cors, {
+  origin: '*' //take domain front-end
+})
 
-    try{
-        const res = await fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=yes`)
+server.get('/climate/:city', async (req, rep) => {
+  const { city } = req.params;
+  const apiKey = process.env.WEATHER_API;
 
-        const data = await res.json()
-        rep.send(data)
-    }catch(err){
-        return rep.code(500).send({
-            error: 'Erro na busca do clima.'
-        })
+  try {
+    const res = await fetch(
+      `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${encodeURIComponent(city)}&aqi=yes&lang=pt-br`
+    );
+    
+    if (!res.ok) {
+      return rep.status(404).send({ error: 'Cidade não encontrada!' });
     }
-})
 
-server.listen({
-    host: '0.0.0.0',
-    port: process.env.port ?? 3000,
-})
+    const data = await res.json();
+    return rep.send(data);
+  }catch (err) {
+    return rep.status(500).send({ error: 'Erro ao buscar clima!' });
+  }
+});
+
+//init server
+const start = async () => {
+  try{
+    server.listen({
+      host: '0.0.0.0',
+      port: process.env.PORT ?? 3000
+    })
+
+    server.log.info('Server running...')
+  } catch(err){
+    server.log.error(err)
+    process.exit(1)
+  }
+}
+
+start()
